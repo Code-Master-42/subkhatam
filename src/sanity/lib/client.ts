b2437b7ab258e66dfc6d5960b2e87ba6b489ddfb
Code-Master-42 <rarain904@gmail.com>
2025-01-19 18:26:01 +0500

    first commit

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,8 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
-
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId:"2q1moi34",
+  dataset:"production",
+  apiVersion:"2021-08-10",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
